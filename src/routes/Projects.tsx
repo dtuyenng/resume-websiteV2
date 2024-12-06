f8d01ed0b9c8ec0bb4f36ae04f9df5eca0f893cc
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-12-06 11:12:53 -0800

    Commit
    Commit

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -1,7 +1,6 @@
 import "../App.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Sidebar from "../components/Sidebar";
 
 function Projects() {
   return (
